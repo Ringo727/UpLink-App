79d1f98d61b7e1723b0665c82f40492477f7d983
Logan Lucas <logs10658@gmail.com>
2025-06-19 01:41:04 -0500

    added xcode template for SwiftUI mobile app

---
+++ frontend/UpLink/ContentView.swift
@@ -0,0 +1,21 @@
+// ContentView for UpLink
+
+// essentially a main file for the frontend
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
