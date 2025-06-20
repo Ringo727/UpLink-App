43c9ddb8556e91f826ef1d917de0788ed84e7a6f
Ringo727 <shibiparfait@gmail.com>
2025-06-20 00:59:16 -0500

    added starte golangci-lint config

---
+++ UpLink/UpLink/UpLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpLinkApp.swift
+//  UpLink
+//
+//  Created by Mitchell Eng on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct UpLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UpLink/UpLinkTests/UpLinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  UpLinkTests.swift
+//  UpLinkTests
+//
+//  Created by Mitchell Eng on 6/19/25.
+//
+
+import Testing
+@testable import UpLink
+
+struct UpLinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
